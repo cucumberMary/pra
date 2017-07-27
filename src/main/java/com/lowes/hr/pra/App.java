e0745949380e28ac33f814668cbf5c1871c0441d
Kiran <kiranreddyamk@gmail.com>
2017-07-26 20:34:22 -0400

    Initial Commit

---
+++ src/main/java/com/lowes/hr/pra/App.java
@@ -0,0 +1,13 @@
+package com.lowes.hr.pra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
